f8f6b352049b017e6ff2624815c7bb494ffc6eae
arletta <912952381@qq.com>
2019-06-09 17:25:20 +0800

    add report

--- src/DESAlgorithm.java
+++ src/DESAlgorithm.java
@@ -6,22 +6,22 @@ public class DESAlgorithm {
 
     private JFileChooser jfc;   //文件选择器
     private File chooseFile;    //待操作文件
-    private StringBuffer sb;    //字符串缓冲
     private FileInputStream fis;//文件输入流
     private FileWriter fw;      //文件输出流
+    private StringBuffer sb;    //字符串缓冲
     private int key;            //密钥
     private int initialization; //初始化向量
 
     public DESAlgorithm() {
         sb = new StringBuffer();
-        key = 20;
-        initialization = 127;
+        key = 20;   //初始化密钥
+        initialization = 127;   //初始化向量
     }
 
     //加密方法
     public void Encrypt() {
         chooseFile = OpenFile("请选择待加密文件");
-        ReadFile();
+        ReadFile(); //从文件中读取明文
         //ECB
         for(int i = 0;i < sb.length();i++) {
             sb.setCharAt(i, (char) ((sb.charAt(i) + key) % 128));
@@ -33,16 +33,15 @@ public class DESAlgorithm {
             sb.setCharAt(i, (char) ((sb.charAt(i) + key) % 128));
             init = sb.charAt(i);
         }
-        WriteFile();
+        WriteFile();    //将密文写回文件
     }
 
     //解密方法
     public void Decode() {
         chooseFile = OpenFile("请选择待解密文件");
-        ReadFile();
+        ReadFile(); //从文件中读取密文
         //CBC
-        int init = initialization;
-        int next = init;
+        int init = initialization, next;
         for(int i = 0;i < sb.length();i++) {
             next = sb.charAt(i);
             sb.setCharAt(i, (char) ((sb.charAt(i) + 128 - key) % 128));
@@ -53,7 +52,7 @@ public class DESAlgorithm {
         for(int i = 0;i < sb.length();i++) {
             sb.setCharAt(i, (char) ((sb.charAt(i) + 128 - key) % 128));
         }
-        WriteFile();
+        WriteFile();    //将明文写回文件
     }
 
     //使用文件选择器打开待操作文件
